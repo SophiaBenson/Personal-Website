0ee2948a59d5138e6cbd44e35637605d79d825a8
Sophia Benson <sophiabenson01@gmail.com>
2016-11-17 14:54:33 -0600

    server set up

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server is listening on port 3000");
+});
+
+
+
+
+app.get('/', function (req, res) {
+  console.log('In main schedule page');
+  res.sendFile(path.resolve('index.html'));
 });
