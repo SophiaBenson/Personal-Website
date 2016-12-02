3bc3d97a96ffc39ffcc37ac05d973e04f9abf007
Sophia Benson <sophiabenson01@gmail.com>
2016-12-02 17:07:45 -0600

    jquery implementation

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,8 @@
 console.log("chirp");
+$(document).ready(function(){
+  $("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#section1").offset().top},
+        'slow');
+});//end button
+});//end document
